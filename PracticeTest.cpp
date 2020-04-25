395d74b46072c2ab3e647acce105a1d9168cd65b
weitaoli123 <47096222+weitaoli123@users.noreply.github.com>
2020-04-25 14:09:58 -0700

    Update PracticeTest.cpp

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -20,3 +20,10 @@ TEST(PracticeTest, is_simple_palindrome)
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("ba");
+    ASSERT_FALSE(actual);
+}
