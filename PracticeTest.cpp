c4f4d89cddff863af47fc04065dba76651b1fb6e
weitaoli123 <47096222+weitaoli123@users.noreply.github.com>
2020-04-25 14:42:55 -0700

    Update PracticeTest.cpp

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -53,8 +53,8 @@ TEST(PracticeTest, sorts_desecend1)
 {
     Practice obj;
     int one = 1;
-    int two = 1;
-    int three = 2;
+    int two = 2;
+    int three = 3;
     obj.sortDescending(one,two,three);
     ASSERT_GE(one,two);
     ASSERT_GE(two,three);
